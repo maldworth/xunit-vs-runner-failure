38f3c00003476a7575944553061d1df36cba8177
maldworth <m@maldworth.com>
2018-04-22 21:21:57 -0400

    Reproduction of XUnit failure

---
+++ Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Class1 : IClassFixture<TestServerFixture>
+    {
+        private readonly TestServerFixture _fixture;
+
+        public Class1(TestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task GetValues()
+        {
+            var result = await _fixture.Client.GetStringAsync("api/values");
+
+            Assert.NotEmpty(result);
+        }
+    }
+}
